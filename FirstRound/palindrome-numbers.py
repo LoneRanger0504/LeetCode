f10f9f1ced366da6682bdda021da5ae285db8a24
LoneRanger0504 <1092811022@qq.com>
2018-08-01 23:54:39 +0800

    增加思路说明
    Signed-off-by: Lone Ranger <1092811022@qq.com>

--- FirstRound/palindrome-numbers.py
+++ FirstRound/palindrome-numbers.py
@@ -3,6 +3,7 @@ class Solution:
         """
         :type x: int
         :rtype: bool
+        直接将int转为str,对str 倒序输出即可，使用str[::-1]表示从最后一个元素倒序排列
         """
         x_str = str(x)
         x_reverse = x_str[::-1]
