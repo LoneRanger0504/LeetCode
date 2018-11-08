da6e3dfeb004dc8721fd5e746e7b19fc5a99df03
LoneRanger0504 <1092811022@qq.com>
2018-11-08 11:16:26 +0800

    find-pivot-index finished

--- FirstRound/Easy/find-pivot-index.py
+++ FirstRound/Easy/find-pivot-index.py
@@ -1,20 +1,36 @@
+"""
+给定一个整数类型的数组 nums，请编写一个能够返回数组“中心索引”的方法。
+
+我们是这样定义数组中心索引的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+
+如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
+"""
 class Solution:
     def pivotIndex(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        """
+        使用两个值来保存元素左边和右边的和，这里的左初始值为0
+        从第一个元素开始，先更新右边和，再原有基础上减去当前元素即可
+        与左边和比较，相等返回第一个。不相等更新左边和，加上当前元素
+        """
         nums_len = len(nums)
-        if nums_len < 3:
-            return -1
-        left = nums[0]
-        right = 0
-        for i in range(nums_len - 1, 1, -1):
-            right += nums[i]
-        for i in range(0, nums_len - 1):
+        left = 0
+        right = sum(nums[i] for i in range(nums_len))
+
+        for i in range(nums_len):
+            right = right - nums[i]
             if left != right:
                 left = left + nums[i]
-                right = right - nums[i + 1]
             else:
                 return i
         return -1
+
+
+if __name__ == '__main__':
+    input_list = [-1, -1, 0, 1, 1, 0]
+    solution = Solution()
+    result = solution.pivotIndex(input_list)
+    print(result)
