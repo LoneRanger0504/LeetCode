b2501ec234a05c76bfdd3a36ed00078e774fca7d
Lone Ranger <1092811022@qq.com>
2019-03-20 00:07:33 +0800

    path-sum && search-in-a-binary-search-tree

---
+++ FirstRound/Easy/path-sum.py
@@ -0,0 +1,45 @@
+"""
+给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+说明: 叶子节点是指没有子节点的节点。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        """
+        还是分左右子树递归，每次先判断当前节点是否为空
+        当root节点左右子树都为空，即达到了叶子节点，判断当前的val是否等于sum
+        每次递归时，更新root为左/右节点, sum更新为sum-root.val
+        :param root:
+        :param sum:
+        :return:
+        """
+        if not root:
+            return False
+        if root.left == None and root.right == None:
+            return root.val == sum
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+
+
+if __name__ == '__main__':
+    root = TreeNode(5)
+    root.left = TreeNode(4)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(11)
+    root.left.left.left = TreeNode(7)
+    root.left.left.right = TreeNode(2)
+    root.right.left = TreeNode(13)
+    root.right.right = TreeNode(4)
+    root.right.right.right = (1)
+    solution = Solution()
+    res = solution.hasPathSum(root, 22)
+    print(res)
---
+++ FirstRound/Easy/search-in-a-binary-search-tree.py
@@ -0,0 +1,40 @@
+"""
+给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        """
+        利用二叉搜索树的性质，左子树的值小于根节点的值，右子树的值大于根节点的值，递归查找即可
+        :param root:
+        :param val:
+        :return:
+        """
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        if root.val < val:
+            return self.searchBST(root.right, val)
+
+
+if __name__ == '__main__':
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(7)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+    solution = Solution()
+    res = solution.searchBST(root, 5)
+    print(res)
